30ac1f25159b1263f78bdb0c60e24ba07bc5984c
Aurélien DELAMARRE <3996456+lenoxys@users.noreply.github.com>
2026-02-27 23:48:06 -0800

    refactor: rewrite entrypoint with runtime PUID/PGID, privilege drop via su-exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,25 @@
 
 export TERM=xterm
 
-if [ -z "$(ls -A /home/rtorrent/rtorrent/config.d/)" ]; then
-    cp -r /home/rtorrent/.rtorrent/config.d/* /home/rtorrent/rtorrent/config.d/
-fi 
+PUID=${PUID:-568}
+PGID=${PGID:-568}
 
-chown -R rtorrent:rtorrent /home/rtorrent/rtorrent/
-rtorrent
+# Update rtorrent user/group to match requested UID/GID
+if [ "$(id -u rtorrent)" != "$PUID" ] || [ "$(id -g rtorrent)" != "$PGID" ]; then
+    deluser rtorrent 2>/dev/null
+    delgroup rtorrent 2>/dev/null
+    addgroup -g "$PGID" rtorrent
+    adduser -S -u "$PUID" -G rtorrent -h /home/rtorrent rtorrent
+fi
+
+# Seed default configs on first run
+if [ -z "$(ls -A /home/rtorrent/rtorrent/config.d/ 2>/dev/null)" ]; then
+    cp -r /home/rtorrent/.rtorrent/config.d/* /home/rtorrent/rtorrent/config.d/
+fi
+
+# Fix ownership for runtime directories
+chown -R rtorrent:rtorrent /home/rtorrent/
+chown rtorrent:rtorrent /completed_downloads/
+
+# Drop privileges and start rtorrent
+exec su-exec rtorrent rtorrent
